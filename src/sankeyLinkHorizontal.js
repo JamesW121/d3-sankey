cd5af188b187a80e05b030f7b26792e277f9da56
Tim Brock <info@datatodisplay.com>
2018-06-12 16:05:00 +0100

    Add a tiny offset to y1 value in sankeyLinkHorizontal when it equals y0
    - Means gradients will still show for horizontal lines without having to use userSpaceOnUse gradientUnits

--- src/sankeyLinkHorizontal.js
+++ src/sankeyLinkHorizontal.js
@@ -5,7 +5,9 @@ function horizontalSource(d) {
 }
 
 function horizontalTarget(d) {
-  return [d.target.x0, d.y1];
+  // If y1 equals y0 add a tiny offset so gradients work with userSpaceOnUse
+  // See https://stackoverflow.com/questions/21638169/svg-line-with-gradient-stroke-wont-display-straight
+  return [d.target.x0, d.y1 + (d.y1 === d.y0 ? 0.0001 : 0)];
 }
 
 export default function() {
