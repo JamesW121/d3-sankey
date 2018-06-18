f25dc325d632fb3c95edd389bc6941abfc45da93
Tim Brock <info@datatodisplay.com>
2018-06-18 12:30:43 +0100

    Expand gradientUnits fix to include y0 approx = y1 as well as exactly equals

--- src/sankeyLinkHorizontal.js
+++ src/sankeyLinkHorizontal.js
@@ -1,13 +1,21 @@
 import {linkHorizontal} from "d3-shape";
 
+var MIN_OFFSET = 0.001;
+
 function horizontalSource(d) {
   return [d.source.x1, d.y0];
 }
 
 function horizontalTarget(d) {
-  // If y1 equals y0 add a tiny offset so gradients work with userSpaceOnUse
+  // If y1 approximately equals y0, add a pixel offset so gradients work with default gradientUnits
   // See https://stackoverflow.com/questions/21638169/svg-line-with-gradient-stroke-wont-display-straight
-  return [d.target.x0, d.y1 + (d.y1 === d.y0 ? 0.0001 : 0)];
+  var y1 = d.y1;
+  var height = y1 - d.y0;
+  if (Math.abs(height) < MIN_OFFSET) {
+    if (height >= 0) y1 = d.y0 + MIN_OFFSET;
+    else y1 = d.y0 - MIN_OFFSET;
+  }
+  return [d.target.x0, y1];
 }
 
 export default function() {
